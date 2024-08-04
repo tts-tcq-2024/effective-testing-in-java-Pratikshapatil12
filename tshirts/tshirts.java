7a2d693b0c4cb362d5d2c7016b08c3eaa12c47b4
Pratikshapatil12 <100845044+Pratikshapatil12@users.noreply.github.com>
2024-08-04 17:41:09 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -1,19 +1,20 @@
 public class Tshirts {
     static String size(int cms) {
-        if(cms < 38) {
+        if (cms < 38) {
             return "S";
-        } else if(cms > 38 && cms < 42) {
+        } else if (cms <= 42) { // Fixed boundary condition
             return "M";
         } else {
             return "L";
         }
     }
 
-    public static void main(String[] args) { 
-        assert(size(37) == "S");
-        assert(size(38) == "M");
-        assert(size(40) == "M");
-        assert(size(43) == "L");
+    public static void main(String[] args) {
+        assert("S".equals(size(37)));
+        assert("M".equals(size(38)));
+        assert("M".equals(size(40)));
+        assert("L".equals(size(43)));
+        assert("L".equals(size(100))); // Additional test case
         System.out.println("All is well (maybe!)");
     }
 }
